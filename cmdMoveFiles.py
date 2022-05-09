011f6e26ca543c929662bc3f1b3e78d91ff8f353
Hester <h.steyn@sanbi.org.za>
2022-05-09 14:25:44 +0200

    cmd works

--- cmdMoveFiles.py
+++ cmdMoveFiles.py
@@ -8,13 +8,13 @@ import click
 
 
 
-@click.command()
-@click.option('--file',  
-                help='File name with image names and families e.g. ImageFamily.csv.')
-@click.option('--source',  
-                help='Folder where unsorted images are.')
-@click.option('--dest', 
-                help='The folder where images would be moved to')
+# @click.command()
+# @click.option('--file',  
+#                 help='File name with image names and families e.g. ImageFamily.csv.')
+# @click.option('--source',  
+#                 help='Folder where unsorted images are.')
+# @click.option('--dest', 
+#                 help='The folder where images would be moved to')
 
 
 # open csv file and iterate files
@@ -33,7 +33,7 @@ def moveFiles(file, source, dest):
             reader = csv.DictReader(csvfile)
             for row in reader:
                 records.append(row)
-        #print(records)
+        print(records)
     except Exception as e:
         print('Error reading file')
         print(e)
@@ -50,21 +50,24 @@ def moveFiles(file, source, dest):
 
         familyFolder = os.path.join(dest, familyName)
         # print(imageName)
-    #construct full file path
+        #construct full file path
         sourceFile = os.path.join(source, imageName)
         if exists(sourceFile):
             print("source is:", sourceFile)
             destination = os.path.join(familyFolder, imageName)
             print("destination is:", destination)
 
-        #test if family folder exists - if not create and then move
+            #test if family folder exists - if not create and then move
             if not os.path.exists(familyFolder):
                 os.mkdir(familyFolder)
-        #move file
+            #move file
             shutil.move(sourceFile, destination) 
             print('Moved:', image['Barcode'])
+
+        else:
+            print("Could not find image", image['Barcode'])
     
 # if __name__ == '__main__':
 #     moveFiles()
 
-# moveFiles('ImageFamily.csv', 'C:/NSCF/ImageMove', 'C:/NSCF/ImageMove/Families')
+moveFiles('ImageFamily.csv', 'C:/NSCF/ImageMove', 'C:/NSCF/ImageMove/Families')
